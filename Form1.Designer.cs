f0cfc78a40da0767c51460647f676af32cbaae5b
a7las <a7las@DESKTOP-7SGDAA8>
2023-12-20 15:24:20 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace OtoSpace
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btngiris = new System.Windows.Forms.Button();
+            this.btnucret = new System.Windows.Forms.Button();
+            this.btnkayıt = new System.Windows.Forms.Button();
+            this.splitter3 = new System.Windows.Forms.Splitter();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.splitter4 = new System.Windows.Forms.Splitter();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtad = new System.Windows.Forms.TextBox();
+            this.txttc = new System.Windows.Forms.TextBox();
+            this.txttel = new System.Windows.Forms.TextBox();
+            this.txtadres = new System.Windows.Forms.TextBox();
+            this.txtmarka = new System.Windows.Forms.TextBox();
+            this.txtmodel = new System.Windows.Forms.TextBox();
+            this.txtrenk = new System.Windows.Forms.TextBox();
+            this.txtplaka = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btngiris
+            // 
+            this.btngiris.Location = new System.Drawing.Point(56, 387);
+            this.btngiris.Name = "btngiris";
+            this.btngiris.Size = new System.Drawing.Size(152, 68);
+            this.btngiris.TabIndex = 0;
+            this.btngiris.Text = "Araç Girişi al";
+            this.btngiris.UseVisualStyleBackColor = true;
+            this.btngiris.Click += new System.EventHandler(this.btngiris_Click);
+            // 
+            // btnucret
+            // 
+            this.btnucret.Location = new System.Drawing.Point(257, 387);
+            this.btnucret.Name = "btnucret";
+            this.btnucret.Size = new System.Drawing.Size(152, 68);
+            this.btnucret.TabIndex = 1;
+            this.btnucret.Text = "Ücretlendir";
+            this.btnucret.UseVisualStyleBackColor = true;
+            this.btnucret.Click += new System.EventHandler(this.btnucret_Click);
+            // 
+            // btnkayıt
+            // 
+            this.btnkayıt.Location = new System.Drawing.Point(805, 440);
+            this.btnkayıt.Name = "btnkayıt";
+            this.btnkayıt.Size = new System.Drawing.Size(155, 68);
+            this.btnkayıt.TabIndex = 2;
+            this.btnkayıt.Text = "Üye Kaydı Oluştur";
+            this.btnkayıt.UseVisualStyleBackColor = true;
+            this.btnkayıt.Click += new System.EventHandler(this.btnkayıt_Click);
+            // 
+            // splitter3
+            // 
+            this.splitter3.Location = new System.Drawing.Point(0, 0);
+            this.splitter3.Name = "splitter3";
+            this.splitter3.Size = new System.Drawing.Size(504, 520);
+            this.splitter3.TabIndex = 5;
+            this.splitter3.TabStop = false;
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(159, 312);
+            this.txt1.Multiline = true;
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(189, 25);
+            this.txt1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(87, 312);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Plaka";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(159, 107);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(189, 182);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // splitter4
+            // 
+            this.splitter4.Location = new System.Drawing.Point(504, 0);
+            this.splitter4.Name = "splitter4";
+            this.splitter4.Size = new System.Drawing.Size(295, 520);
+            this.splitter4.TabIndex = 11;
+            this.splitter4.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(583, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 24);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "AdSoyad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(550, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 24);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "TC Numarası";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(511, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(159, 24);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Telefon Numarası";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(610, 165);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 24);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Adres";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(609, 198);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 24);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Marka";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(610, 230);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 24);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Model";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(616, 265);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 24);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Renk";
+            // 
+            // txtad
+            // 
+            this.txtad.Location = new System.Drawing.Point(676, 56);
+            this.txtad.Name = "txtad";
+            this.txtad.Size = new System.Drawing.Size(100, 20);
+            this.txtad.TabIndex = 19;
+            // 
+            // txttc
+            // 
+            this.txttc.Location = new System.Drawing.Point(676, 97);
+            this.txttc.Name = "txttc";
+            this.txttc.Size = new System.Drawing.Size(100, 20);
+            this.txttc.TabIndex = 20;
+            // 
+            // txttel
+            // 
+            this.txttel.Location = new System.Drawing.Point(676, 134);
+            this.txttel.Name = "txttel";
+            this.txttel.Size = new System.Drawing.Size(100, 20);
+            this.txttel.TabIndex = 21;
+            // 
+            // txtadres
+            // 
+            this.txtadres.Location = new System.Drawing.Point(676, 169);
+            this.txtadres.Name = "txtadres";
+            this.txtadres.Size = new System.Drawing.Size(100, 20);
+            this.txtadres.TabIndex = 22;
+            // 
+            // txtmarka
+            // 
+            this.txtmarka.Location = new System.Drawing.Point(676, 202);
+            this.txtmarka.Name = "txtmarka";
+            this.txtmarka.Size = new System.Drawing.Size(100, 20);
+            this.txtmarka.TabIndex = 23;
+            // 
+            // txtmodel
+            // 
+            this.txtmodel.Location = new System.Drawing.Point(676, 234);
+            this.txtmodel.Name = "txtmodel";
+            this.txtmodel.Size = new System.Drawing.Size(100, 20);
+            this.txtmodel.TabIndex = 24;
+            // 
+            // txtrenk
+            // 
+            this.txtrenk.Location = new System.Drawing.Point(676, 269);
+            this.txtrenk.Name = "txtrenk";
+            this.txtrenk.Size = new System.Drawing.Size(100, 20);
+            this.txtrenk.TabIndex = 25;
+            // 
+            // txtplaka
+            // 
+            this.txtplaka.Location = new System.Drawing.Point(676, 301);
+            this.txtplaka.Name = "txtplaka";
+            this.txtplaka.Size = new System.Drawing.Size(100, 20);
+            this.txtplaka.TabIndex = 26;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(609, 297);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(55, 24);
+            this.label9.TabIndex = 27;
+            this.label9.Text = "Plaka";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(800, 23);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(173, 185);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 28;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(836, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 24);
+            this.button1.TabIndex = 29;
+            this.button1.Text = "Fotoğraf Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(972, 520);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtplaka);
+            this.Controls.Add(this.txtrenk);
+            this.Controls.Add(this.txtmodel);
+            this.Controls.Add(this.txtmarka);
+            this.Controls.Add(this.txtadres);
+            this.Controls.Add(this.txttel);
+            this.Controls.Add(this.txttc);
+            this.Controls.Add(this.txtad);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.btnkayıt);
+            this.Controls.Add(this.btnucret);
+            this.Controls.Add(this.btngiris);
+            this.Controls.Add(this.splitter4);
+            this.Controls.Add(this.splitter3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btngiris;
+        private System.Windows.Forms.Button btnucret;
+        private System.Windows.Forms.Button btnkayıt;
+        private System.Windows.Forms.Splitter splitter3;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Splitter splitter4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtad;
+        private System.Windows.Forms.TextBox txttc;
+        private System.Windows.Forms.TextBox txttel;
+        private System.Windows.Forms.TextBox txtadres;
+        private System.Windows.Forms.TextBox txtmarka;
+        private System.Windows.Forms.TextBox txtmodel;
+        private System.Windows.Forms.TextBox txtrenk;
+        private System.Windows.Forms.TextBox txtplaka;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace OtoSpace
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btngiris_Click(object sender, EventArgs e)
+        {
+            string connectionString="";
+            string plaka = txt1.Text;
+            DateTime girisSaati = DateTime.Now;
+
+            if (!string.IsNullOrEmpty(plaka))
+            {
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+
+                    string query = "";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+
+                        command.Parameters.AddWithValue("", plaka);
+                        command.Parameters.AddWithValue("", girisSaati);
+
+
+                        try
+                        {
+                            connection.Open();
+                            int rowsAffected = command.ExecuteNonQuery();
+                            if (rowsAffected > 0)
+                            {
+                                MessageBox.Show("Plaka ve giriş saati başarıyla kaydedildi.");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Hata oluştu: " + ex.Message);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen bir plaka girin.");
+            }
+        }
+
+        private void btnucret_Click(object sender, EventArgs e)
+        {
+            string connectionString = "";
+
+            string plaka = txt1.Text;
+            DateTime cikissaati = DateTime.Now;
+            decimal ucret = 0;
+
+            if (!string.IsNullOrEmpty(plaka))
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string query = "";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("", plaka);
+
+                        try
+                        {
+                            connection.Open();
+                            object result = command.ExecuteScalar();
+
+                            if (result != null)
+                            {
+                                DateTime girisSaati = Convert.ToDateTime(result);
+                                TimeSpan parkSuresi = cikissaati - girisSaati;
+
+                                
+                                ucret = Convert.ToDecimal(Math.Ceiling(parkSuresi.TotalHours)) * 10;
+
+                                //MessageBox oluşturulacak
+                            }
+                            else
+                            {
+                                MessageBox.Show("Giriş yapılmış bir kayıt bulunamadı.");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Hata oluştu: " + ex.Message);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen bir plaka girin.");
+            }
+        }
+
+        private void btnkayıt_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;";
+
+            string adSoyad = txtad.Text;
+            string tcNo = txttc.Text;
+            string telefon = txttel.Text;
+            string adres = txtadres.Text;
+            string arabaMarkasi = txtmarka.Text;
+            string model = txtmodel.Text;
+            string renk = txtrenk.Text;
+            string plaka = txtplaka.Text;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO ArabaBilgileri (AdSoyad, TcNo, Telefon, Adres, ArabaMarkasi, Model, Renk, Plaka) " +
+                               "VALUES (@AdSoyad, @TcNo, @Telefon, @Adres, @ArabaMarkasi, @Model, @Renk, @Plaka)";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@AdSoyad", adSoyad);
+                    command.Parameters.AddWithValue("@TcNo", tcNo);
+                    command.Parameters.AddWithValue("@Telefon", telefon);
+                    command.Parameters.AddWithValue("@Adres", adres);
+                    command.Parameters.AddWithValue("@ArabaMarkasi", arabaMarkasi);
+                    command.Parameters.AddWithValue("@Model", model);
+                    command.Parameters.AddWithValue("@Renk", renk);
+                    command.Parameters.AddWithValue("@Plaka", plaka);
+
+                    try
+                    {
+                        connection.Open();
+                        int rowsAffected = command.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Kayıt başarıyla eklendi.");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Hata oluştu: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // VIP üye fotoğrafı eklemek için kod yazılacak
+        }
+    }
+    }
+}
